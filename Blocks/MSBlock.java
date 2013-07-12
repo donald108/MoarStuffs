9a7884f4f5fcb3f2e3ccf828643ace8b0965eea3
glassesofdoom <Glassesofdoom@gmail.com>
2013-07-12 16:58:08 -0400

    Added a Block

--- Blocks/MSBlock.java
+++ Blocks/MSBlock.java
@@ -1,9 +1,22 @@
 package MoarStuffs.blocks;
+
+import MoarStuffs.lib.id.BlockIds;
+import MoarStuffs.lib.name.BNames;
+import net.minecraft.block.Block;
+
 /*
  *Author: Glassesofdoom (Anthony Anderson)
  *Last edit: 7/12/2013 By: Glassesofdoom (Anthony Anderson)
  *
  */
 public class MSBlock {
+	
+	public static Block copperOre;
+
+	public static void init() {
+		
+		copperOre = new BlockcopperOre(BlockIds.ORECOPPER).setUnlocalizedName(BNames.copperOre);
+		
+	}
 
 }
